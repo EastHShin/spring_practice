cdec322dd857fe41cccf1d59a2ac3f3290c56137
EastHShin <64204666+EastHShin@users.noreply.github.com>
2021-01-02 17:18:51 +0900

    update

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,15 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -24,6 +25,7 @@ public class MemberService {
         memberRepository.save(member);
         return member.getId();
     }
+
     private void ValidateDuplication(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
